8bf22610967f1260a14753d82d2896879867f657
Amarbold Bazarsuren <bazarsurenamarbold@gmail.com>
2026-03-31 20:59:43 +0800

    vite change

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   timeout: 15000,
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('admin_token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
